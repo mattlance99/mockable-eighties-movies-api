de5503801055f662894078f893f61e253c72dd9f
github username <github email address>
2019-04-19 15:30:10 -0400

    support for conflicting port

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,15 @@
 class MoviesController < ApplicationController
+
+  def new
+    @movie = Movie.new
+  end
+
+  def index
+    render json: Movie.all
+  end
+
+  def show
+    render json: Movie.find_by(id: params['id'])
+  end
+
 end
